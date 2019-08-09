e74ba15f5ae464b692e14a9f746f36187532b663
seyounglee <lsy_65@naver.com>
2019-08-09 18:14:51 +0900

    Updates

---
+++ precache-manifest.79fd148e864549ab8e5a88c5221dd78d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7430f5e63db83588c3fb151f6e7102",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "9ddad67d7cdc20b19246",
+    "url": "/todo-list/static/css/main.766e36f5.chunk.css"
+  },
+  {
+    "revision": "8f8faabc9ad4e2305dca",
+    "url": "/todo-list/static/js/2.d1dc1951.chunk.js"
+  },
+  {
+    "revision": "9ddad67d7cdc20b19246",
+    "url": "/todo-list/static/js/main.6c82c850.chunk.js"
+  },
+  {
+    "revision": "ca797cc406ded84cf733",
+    "url": "/todo-list/static/js/runtime~main.41b69a58.js"
+  }
+]);
